fb3a9136b389769b28d20d652b99d98c81c16e62
Javier <javier.gutierrez.cruz@gmail.com>
2020-09-22 18:08:25 -0500

    [Fix]Add print numbers function still not working properly

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,37 @@
 const temadark = document.getElementById("dark");
 const temaretro = document.getElementById("retro");
 const temabody = document.getElementById("containAll");
-const screen = document.getElementById("screen1")
-const screen1 = document.getElementById("screen2")
+let screen1 = document.getElementById("screen1")
+let screen2 = document.getElementById("screen2")
 const buttons = document.querySelectorAll("#buttons a");
 
 
 for (const button of buttons) {
     button.addEventListener("click", function(e){
+        e.preventDefault
+        let letters = (e.target.dataset.key)[0]
+        let numbers = (e.target.dataset.key)[1]
+        let sc1 = screen1.textContent.length;
+        let sc2 = screen2.textContent.length;
+        console.log(letters);
+        console.log(numbers);
 
+        if(e.target.dataset.key == "reset"){
+            console.log("reset")
+            screen1.textContent = "";
+            screen2.textContent = "";
+        }
+        if (screen1.textContent.indexOf(letters) != -1 && sc1 < 10)
+            swal(letters + " Esta repetida", "El orden debe ser: ABCDEFGHIJ!", "error");
+        if (screen2.textContent.indexOf(numbers) != -1 && sc2 < 10)
+            swal(numbers + " Esta repetido", "El orden debe ser: 1234567890", "error");
+
+        if (sc1 < 10){
+            screen1.textContent = screen1.textContent + letters;
+        }  
+        if (sc2 < 10){
+            screen2.textContent = screen2.textContent + numbers;
+        }  
     })
 }
 
