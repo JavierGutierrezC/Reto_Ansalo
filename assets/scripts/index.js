aaf50420cc1005ff897f2df7d349054bcc209cb9
Javier <javier.gutierrez.cruz@gmail.com>
2020-09-23 22:20:03 -0500

    [Fixed]Reto Ansalo has been fixed

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,27 +13,36 @@ for (const button of buttons) {
         let numbers = (e.target.dataset.key)[1]
         let sc1 = screen1.textContent.length;
         let sc2 = screen2.textContent.length;
-        console.log(letters);
-        console.log(numbers);
-
-       
+ 
         if (screen1.textContent.indexOf(letters) != -1 && sc1 < 10)
             swal(letters + " Esta repetida", "El orden debe ser: ABCDEFGHIJ!", "error");
         if (screen2.textContent.indexOf(numbers) != -1 && sc2 < 10)
             swal(numbers + " Esta repetido", "El orden debe ser: 1234567890", "error");
 
-        if (sc1 < 10){
+        if (sc1 < 1 && letters == "A"){
+            screen1.textContent = screen1.textContent + letters; 
+        } 
+        else if (sc1 >= 1 && sc1 < 10 && (letters.charCodeAt(0) == (parseInt(screen1.textContent.charCodeAt(sc1 -1))+1))){
             screen1.textContent = screen1.textContent + letters;
-        }  
-        if (sc2 < 10){
+        }
+        else if (sc2 < 1 && sc1 == 10 && numbers == "1"){
             screen2.textContent = screen2.textContent + numbers;
-        }  
-        
-        if(e.target.dataset.key == "reset"){
+        }else if (sc2 >= 1 && sc1 == 10 && parseInt(numbers) == parseInt(sc2)+1){
+            screen2.textContent = screen2.textContent + numbers;
+        } else if (sc2 >=9 && sc1 == 10 && numbers == "0"){
+            screen2.textContent = screen2.textContent + numbers;
+        } else if(e.target.dataset.key == "reset"){
             console.log("reset")
             screen1.textContent = "";
             screen2.textContent = "";
         }
+        else if (sc1 < 10){
+            swal(letters + " No es la letra siguiente", "El orden debe ser: ABCDEFGHIJ!", "error");
+        }
+        else if (sc2 < 10 && sc1 == 10){
+            swal(numbers + " No es la letra siguiente", "El orden debe ser: ABCDEFGHIJ!", "error");
+        }
+        
     })
 }
 
